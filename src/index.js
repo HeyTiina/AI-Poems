5e31ff48b12bbbfc7d6034998ec0ed559b661d7c
HeyTiina <130788330+HeyTiina@users.noreply.github.com>
2024-05-23 18:08:37 +0200

    ai generator for everything!

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-function displayPoem(response) {
-  new Typewriter("#poem", {
+function displayText(response) {
+  new Typewriter("#text", {
     strings: response.data.answer,
     autoStart: true,
     delay: 2,
@@ -7,32 +7,33 @@ function displayPoem(response) {
   });
 }
 
-async function generatePoem(event) {
+async function generateText(event) {
   event.preventDefault();
 
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "2a830c1f5845c71a9b8c68a49820t94o";
   let context = `
-    You are a funny Poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML and separate each line with a <br />.
-    Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'Tina AI' inside a <strong> element at the end of the poem and NOT at the beginning.
+    You are an expert and love to help people with their questions. Generate a text about the topic of the instructions of the user. 
+    The text has to be not more than 5 lines and concrete.
+    Sign the text with 'Tina AI' inside a <strong> element at the end of the text and NOT at the beginning.
   `;
-  let prompt = `User instructions: Generate a funny poem about ${instructionsInput.value}`;
+  let prompt = `User instructions: Generate a text about ${instructionsInput.value}`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${encodeURIComponent(
     prompt
   )}&context=${encodeURIComponent(context)}&key=${apiKey}`;
 
-  let poemElement = document.querySelector("#poem");
-  poemElement.classList.remove("hidden");
-  poemElement.innerHTML = `<div class="generating">⏳ Generating a funny poem about ${instructionsInput.value}</div>`;
+  let TextElement = document.querySelector("#text");
+  TextElement.classList.remove("hidden");
+  TextElement.innerHTML = `<div class="generating">⏳ Generating text about ${instructionsInput.value}</div>`;
 
   try {
     let response = await axios.get(apiURL);
-    displayPoem(response);
+    displayText(response);
   } catch (error) {
-    poemElement.innerHTML = `<div class="error">❌ Error generating poem. Please try again later.</div>`;
-    console.error("Error generating poem:", error);
+    TextElement.innerHTML = `<div class="error">❌ Error generating. Please try again later.</div>`;
+    console.error("Error generating:", error);
   }
 }
 
-let poemFormElement = document.querySelector("#poem-generator-form");
-poemFormElement.addEventListener("submit", generatePoem, { once: true });
+let TextFormElement = document.querySelector("#text-generator-form");
+TextFormElement.addEventListener("submit", generateText);
